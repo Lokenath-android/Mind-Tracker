f72bc9479a3ab96ba6100e95aec1844fa251cf30
Lokenath-android <apughar27@gmail.com>
2026-02-22 10:44:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('fetch', event => {});
